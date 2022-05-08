e3b928d759adc83961f5e1de650c78ee78c6d45f
trandinhthangv1 <trandinhthangv1@gmail.com>
2022-05-08 23:53:33 +0700

    Design default layout

--- src/layouts/Default/Default.jsx
+++ src/layouts/Default/Default.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import classNames from 'classnames/bind';
 import Header from '~/layouts/Header';
 import Sidebar from '~/layouts/Sidebar';
+import styles from './Default.module.scss';
+
+const cx = classNames.bind(styles);
 
 const Default = ({ children }) => {
   return (
-    <div>
+    <div className={cx('wrapper')}>
       <Header />
-      <Sidebar />
-      {children}
+      <div className={cx('container')}>
+        {' '}
+        <Sidebar />
+        <div className={cx('content')}> {children}</div>
+      </div>
     </div>
   );
 };
