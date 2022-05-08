09cc8716c8b19e6ff7107d081cbcd477c0cbce42
trandinhthangv1 <trandinhthangv1@gmail.com>
2022-05-08 16:24:50 +0700

    Add GlobalStyles

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from '~/App';
 import { store } from '~/app/store';
-import reportWebVitals from '~/reportWebVitals';
+import GlobalStyles from '~/components/GlobalStyles/GlobalStyles';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -11,12 +11,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <GlobalStyles>
+        <App />
+      </GlobalStyles>
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
