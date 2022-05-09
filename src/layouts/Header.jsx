1bf93443e030185e8f429182d810273ccb84d541
trandinhthangv1 <trandinhthangv1@gmail.com>
2022-05-09 21:28:49 +0700

    Design search ui

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import classNames from 'classnames/bind';
+import { RiCloseCircleFill } from 'react-icons/ri';
+import { AiOutlineLoading3Quarters, AiOutlineSearch } from 'react-icons/ai';
 import styles from './Header.module.scss';
+import { ReactComponent as Logo } from '~/assets/images/logo.svg';
 
 const cx = classNames.bind(styles);
 
 const Header = () => {
   return (
     <header className={cx('wrapper')}>
-      <div className={cx('inner')}>Header</div>
+      <div className={cx('inner')}>
+        <div className={cx('logo')}>
+          <Logo />
+        </div>
+        <div className={cx('search')}>
+          <input placeholder="Tìm kiếm tài khoản và video" spellCheck="false" />
+          <button className={cx('clear')}>
+            <RiCloseCircleFill size={15} />
+          </button>
+          <AiOutlineLoading3Quarters className={cx('loading')} size={15} />
+          <span></span>
+          <button className={cx('search-btn')}>
+            <AiOutlineSearch size={24} />
+          </button>
+        </div>
+        <div className={cx('actions')}>Actions</div>
+      </div>
     </header>
   );
 };
